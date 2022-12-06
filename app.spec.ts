97cb3b11a91e3eea9a917b0deee887e62652cfd0
Nattzor <74113960+Nattzor@users.noreply.github.com>
2022-12-06 15:43:53 +0100

    creating tests

--- app.spec.ts
+++ app.spec.ts
@@ -1,15 +1,21 @@
+import { default as request } from 'supertest';
+import makeApp from './app'
+import nock from 'nock'
 
-import jest from 'jest';
-import makeApp from './app';
-import nock from 'nock';
+const getAllExercises = jest.fn()
+const getExerciseById = jest.fn()
+const createExercise = jest.fn()
 
-const createExercise = jest.fn();
-const getAllExercises=jest.fn();
-const getExerciseById=jest.fn();
+const app = makeApp(createExercise, getAllExercises, getExerciseById);
+const validExercise = {
+    startTime: "2022-12-06",
+    durationInSeconds: 5,
+    activityType: "running"
+}
+describe("GET /exercise/:id", () => {
 
-const app=makeApp(createExercise,getAllExercises,getExerciseById);
-
-const validExercise =   {startTime:'2022-06-08' ,
-    durationInSeconds: 15,
-activityType: "running"}
-
+    it("should return 400 if invalid mongo id is provided", async () => {
+        const response = await request(app).get("/exercise/hejhej");
+        expect(response.statusCode).toBe(400);
+    })
+})
